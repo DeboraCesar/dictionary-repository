5d8d4da06aa2d61a1a6c770c4ecdc7a3c188b16f
DeboraCesar <deboravieira.cesar@gmail.com>
2023-08-22 18:41:07 +0100

    Added Dictionary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import './App.css';
+import logo from "./name.png";
+import background from "./background.jpg";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a
+          href="https://main--spectacular-kleicha-8e0454.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          title="Portfolio"
+        >
+          <img
+            src={logo}
+            className="logo-debora img-fluid"
+            alt="Logo Debora Cesar"
+          />
+        </a>
       </header>
+      <div
+        className="Dictionary-body"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <p>
+            Designed and coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/deboracesar/"
+              target="_blank"
+              rel="noreferrer"
+              title="LinkedIn"
+            >
+              Debora Cesar
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/DeboraCesar/dictionary-repository"
+              target="_blank"
+              rel="noreferrer"
+              title="GitHub Repository"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+import Results from "./Results";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    // console.log(response.data);
+    // console.log(response.data.meanings[0].definition);
+    setResults(response.data);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    // Used API Documentation: https://www.shecodes.io/learn/apis/dictionary
+    // API Used on the SheCodes video: https://dictionaryapi.dev/
+
+    let apiKey = "ba0o0512ad425cabt4be3543c45790fa";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          onChange={handleKeywordChange}
+          type="search"
+          autoFocus={true}
+          autoComplete="Off"
+        />
+      </form> 
+      <Results results={results}/>
+    </div>
+  );
+}
